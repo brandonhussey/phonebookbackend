76990d3a74d1479bd9de7d22878574cd3d242369
brandonhussey <brandon_hussey@icloud.com>
2022-01-29 17:35:03 -0400

    uibuild

--- models/person.js
+++ models/person.js
@@ -21,7 +21,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{4,}/;
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
   },
 });
 
